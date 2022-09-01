022cef7ee26e58e4bef46fe5b6278473b2e6ccc0
KaiSeow <kaii4112000@gmail.com>
2022-09-01 22:05:52 +0800

    .

--- pages/learn.vue
+++ pages/learn.vue
@@ -317,6 +317,18 @@ export default {
 </script>
 
 <style>
+body:fullscreen {
+  overflow: scroll !important;
+}
+body:-ms-fullscreen {
+  overflow: scroll !important;
+}
+body:-webkit-full-screen {
+  overflow: scroll !important;
+}
+body:-moz-full-screen {
+  overflow: scroll !important;
+}
 .btn:hover {
   transform: scale(1.2);
 }
